77021344614eee32ac9a5b23d45d170eb53e22a5
LIUHUAN <liu2012huan@126.com>
2019-10-13 00:08:25 +0800

    [update]implement binary search

--- src/lisp/1.3/binary_search.scm
+++ src/lisp/1.3/binary_search.scm
@@ -1,13 +1,50 @@
 (display "binary search\n")
 
 
+(define (search f a b)
+  (let ((mid-point (average a b)))
+    (if (close-enough? a b) 
+      mid-point
+      (
+        let 
+          ((test-value (f mid-point)))
+          (
+            cond 
+            ((positive? test-value) (search f a mid-point))
+            ((negative? test-value) (search f mid-point b ))
+            (else mid-point)
+          )
+      )
+    )
+  )
+)
+(define (close-enough? x y) 
+  (< (abs (- x y)) 0.001 )
+)
+
+(define (average x y)
+  (/ (+ x y) 2)
+)
 (define (add x y)
     (+ 
       (let ((a 3))  (+ 0 a) ) x y
     )
 )
 
+(define (positive? x)
+  (> x 0)
+)
 
+(define (negative? x)
+  (< x 0)
+)
+
+(define (sign x) 
+  (cond ((positive? x) 1)
+    ((negative? x) -1)
+    (else 0)
+  )
+)
 
 
 
@@ -33,3 +70,5 @@
 )
 
 (displaynewline (add2 100))
+(displaynewline (search (lambda (x) (+ (* x x) (* 2 x ) 1 )) -2 1 ))
+(displaynewline (search sin 2.0 4.0 ))
