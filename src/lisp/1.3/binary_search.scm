b7ef4374050445349caf9bce347c52915bb10f79
LIUHUAN <happygodliu@tencent.com>
2019-10-14 21:21:17 +0800

    [update] binary search for search the x st. f(x) = 0

--- src/lisp/1.3/binary_search.scm
+++ src/lisp/1.3/binary_search.scm
@@ -1,17 +1,39 @@
 (display "binary search\n")
 
+(define (half-interval-method f a b)
+  (let 
+    (
+      (a-value (f a))
+      (b-value (f b))
+    )
+  
+      (cond 
+        (
+          (and (negative? a-value) (positive? b-value))
+          (search f a b)
+        )
+        (
+          (and (negative? b-value) (positive? a-value))
+          (search f b a)
+        )
+        (else 
+          (error "values are not of oppsite sign"  a b)
+        )
+      )
+    )
+)
 
-(define (search f a b)
+(define (search func a b)
   (let ((mid-point (average a b)))
     (if (close-enough? a b) 
       mid-point
       (
         let 
-          ((test-value (f mid-point)))
+          ((test-value (func mid-point)))
           (
             cond 
-            ((positive? test-value) (search f a mid-point))
-            ((negative? test-value) (search f mid-point b ))
+            ((positive? test-value) (search func a mid-point))
+            ((negative? test-value) (search func mid-point b ))
             (else mid-point)
           )
       )
@@ -25,11 +47,11 @@
 (define (average x y)
   (/ (+ x y) 2)
 )
-(define (add x y)
-    (+ 
-      (let ((a 3))  (+ 0 a) ) x y
-    )
-)
+; (define (add x y)
+;     (+ 
+;       (let ((a 3))  (+ 0 a) ) x y
+;     )
+; )
 
 (define (positive? x)
   (> x 0)
@@ -47,28 +69,16 @@
 )
 
 
-
-(define (square x) (* x x))
-(define (f g)
-    (g 2)
-)
-
-(define (displaynewline x) 
+(define (displaynewline x)
   (display x)
   (display "\n")
 )
-(displaynewline ((lambda (x) (* x x)) 2) )
-; (displaynewline (f f))
-(displaynewline (f (lambda (x) (* x (+ 1 x) )) ) ) 
-(displaynewline (add 1 6))
-(define (add2 x )
+; (displaynewline ((lambda (x) (* x x)) 2) )
+; (displaynewline (f (lambda (x) (* x (+ 1 x) )) ) ) 
 
-(+ (let ((x 3)) 
-    (+ x (* x 10))
-    )
-     x)
-)
-
-(displaynewline (add2 100))
-(displaynewline (search (lambda (x) (+ (* x x) (* 2 x ) 1 )) -2 1 ))
-(displaynewline (search sin 2.0 4.0 ))
+; (displaynewline (add2 100))
+; (displaynewline (search (lambda (x) (+ (* x x) (* 2 x ) 1 )) -2 1 ))
+; (displaynewline (search sin 2.0 4.0 ))
+(displaynewline (half-interval-method sin 2.0 4.0 ))
+(displaynewline (half-interval-method (lambda (x) (+ x 2)) -3 4.0 ))
+(displaynewline (search (lambda (x ) (+ x 2)) -3 4.0))
