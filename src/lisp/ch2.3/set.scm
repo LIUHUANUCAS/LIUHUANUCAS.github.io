4a772a8b52f8d59b4282c1f886306081e5b61468
LIUHUAN <liu2012huan@126.com>
2019-11-07 23:50:26 +0800

    [update] set implement

--- src/lisp/ch2.3/set.scm
+++ src/lisp/ch2.3/set.scm
@@ -1,10 +1,21 @@
 (display "set\n")
-(define (element-of-set? x set)
+
+
+(define (element-of-set? x set) ;2.59
 	(cond
 		((null? set) '#f)
 		((equal? x (car set) ) '#t)
 		(else (element-of-set? x (cdr set)))
 	)
+)
+
+(define (union-set set1 set2)
+    (cond 
+        ((null? set1) set2)
+        ((null? set2) set1)
+        ((element-of-set? (car set1) set2) (union-set (cdr set1) set2))
+        (else (cons (car set1) (union-set (cdr set1) set2)))
+    )
 )
 (define (adjoin-set x set)
     (if (element-of-set? x set)
@@ -33,3 +44,4 @@
 (displaynewline (adjoin-set 2 '(2 3 4)))
 (displaynewline (intersection-set '(2) '(2 3 4)))
 (displaynewline (intersection-set '(2 4) '(2 3 4)))
+(displaynewline (union-set '(2 4 1 0 -1) '(2 3 4)))
