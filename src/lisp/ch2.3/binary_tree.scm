8a15dc27a094937b0c0274e77f33a47d7635370b
LIUHUAN <liu2012huan@126.com>
2019-11-14 09:12:43 +0800

    [update] list to binary tree

--- src/lisp/ch2.3/binary_tree.scm
+++ src/lisp/ch2.3/binary_tree.scm
@@ -1,5 +1,36 @@
 (display "binary tree\n")
+(define (list-tree element)
+    (car (partial-tree element (length element)))
+)
+(define (partial-tree elts n)
+    (if (= n 0)
+        (cons '() elts)
+        (let 
+            ((left-size (quotient (- n 1) 2)))
+            (let 
+                ((left-result (partial-tree elts left-size)))
+                (let 
+                    ((left-tree (car left-result))
+                        (non-left-elts (cdr left-result))
+                        (right-size (- n (+ left-size 1)))
+                    )
+                    (let
+                        ((this-entry (car non-left-elts))
+                            (right-result (partial-tree (cdr non-left-elts) 
+                                                            right-size)))
 
+                            (let 
+                                ((right-tree (car right-result))
+                                    (remaining-elts (cdr right-result)))
+                                (cons (make-tree this-entry left-tree right-tree) remaining-elts)
+                            )
+
+                   )
+                )
+            )
+        )
+    )
+)
 (define (lookup given-key set-of-records) 
     (cond 
         ( (null? set-of-records) '#f )
@@ -112,4 +143,5 @@
 
 (displaynewline (tree->list-1 (add-to-tree '() (list  5 3   9 1 7 11))))
 (displaynewline (tree->list-2 (add-to-tree '() (list  5 3   9 1 7 11))))
+(displaynewline (list-tree (list 1 3 5 7 9)))
 
