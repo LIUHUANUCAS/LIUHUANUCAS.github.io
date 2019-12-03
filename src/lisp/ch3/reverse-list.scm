8ecc03086eaf0e81e8e1bffe90780728353470f8
LIUHUAN <happygodliu@tencent.com>
2019-12-03 12:59:05 +0800

    [update] reverse list using set-cdr

---
+++ src/lisp/ch3/reverse-list.scm
@@ -0,0 +1,21 @@
+(display "reverse list  car-cdr \n")
+(define (displaynewline x)
+    (display x)
+    (newline)
+)
+; 3.14
+(define (reverse-list x)
+    (define (loop x y)
+        (if (null? x) 
+            y
+            (let ((temp (cdr x)))
+                (set-cdr! x y)
+                (loop temp x)
+            )
+        )
+    )
+    (loop x '())
+)
+
+(define x (list 1 2 3 4 5))
+(displaynewline (reverse-list x))
