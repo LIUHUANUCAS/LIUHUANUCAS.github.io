800c9b99f94159d931e90c1b8821310fc7646efe
LIUHUAN <liu2012huan@126.com>
2019-12-03 10:14:12 +0800

    [update] set car cdr using append

--- src/lisp/ch3/set-car-cdr.scm
+++ src/lisp/ch3/set-car-cdr.scm
@@ -1,11 +1,36 @@
 (display "set car-cdr \n")
-
-
-
 (define (displaynewline x)
     (display x)
     (newline)
 )
+(define (append x y)
+    (if (null? x)
+        y
+        (cons (car x) (append (cdr x) y))
+    )
+)
+(define (last-pair x)
+    (if (null? (cdr x))
+        x 
+        (last-pair (cdr x))
+    )
+)
+(define (append! x y )
+    (set-cdr! (last-pair x) y)
+    x
+)
+(define (get-new-pair)
+    (cons 'x 'y )
+)
+(define (cons1 x y)
+    (let 
+        ((new (get-new-pair)))
+        (set-car! new x)
+        (set-cdr! new y)
+        new
+    )
+)
+
 (define (test-set-car )
 
     (define x (cons 1 2))
@@ -24,5 +49,15 @@
 )
 
 (test-set-car)
+(displaynewline (cons1 1 2))
+(define x (list 'a 'b))
+(define y (list 'c 'd))
+(define z (append x y))
+(define w (append! x y))
+(displaynewline (append! (list 1 2 ) 3))
+(displaynewline (cdr x))
+(displaynewline z)
+(displaynewline w)
+(displaynewline (cdr x))
 
 
